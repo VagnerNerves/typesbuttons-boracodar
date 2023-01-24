c92e5fb252be3068f35ae455287760a8ec449ba0
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-24 20:18:06 -0300

    feat: Ajusted coloer e fonts in Tawilndcss.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      sansRoboto: ['Roboto', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        zinc: {
+          500: '#6A617F',
+          600: '#3C3748',
+          900: '#0B0911'
+        },
+        purple: {
+          100: '#D9CDF7',
+          400: '#9674E5',
+          500: '#8257E5',
+          800: '#1C162C',
+          900: '#130F1E'
+        }
+      }
+    }
   },
   plugins: []
 }
